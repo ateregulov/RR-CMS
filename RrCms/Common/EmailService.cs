049be96cf1e36c70d75cd0c7f6a6e12a5ac46c9a
sqrter <spworks@ukr.net>
2012-02-05 13:59:45 +0200

    Форма обратной связи (приема заявки)

---
+++ RrCms/Common/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace RrCms.Common
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// Простенький метод отправки через SMTP сервер GMail
+        /// </summary>
+        /// <param name="message"></param>
+        public void SendByGoogle(MailMessage message)
+        {
+            if (message != null)
+            {
+
+                var mailclient = new SmtpClient();
+                mailclient.Host = "smtp.gmail.com";
+                mailclient.Port = 587;
+
+                mailclient.EnableSsl = true;
+
+                //TODO: Хардкодим здесь либо берем из БД настройки 
+                mailclient.Credentials = new System.Net.NetworkCredential(
+                                                 "login",
+                                                 "password");
+                mailclient.Send(message);
+            }
+        }
+    }
+
+}
