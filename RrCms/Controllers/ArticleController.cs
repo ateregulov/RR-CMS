9332bb92c6da42d33128c20786b09068acc497bc
Mike Panin <the.usefull@gmail.com>
2012-02-06 01:06:13 +0400

    Управление статьями для авторизованых пользователей (добавление, редактирование, удаление). Упрощённое отображение статьи.
    Пользователь: usefull
    Пароль: 28081975

---
+++ RrCms/Controllers/ArticleController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RrCms.Controllers.Models;
+using RrCms.Models;
+
+namespace RrCms.Controllers
+{
+    public class ArticleController : Controller
+    {
+        //
+        // GET: /Article/
+        [NeedEditorRole]
+        public ActionResult Index()
+        {
+            using (var db = new ArticleEntities())
+            {
+                return View(db.Articles.ToList());
+            }
+        }
+
+        //
+        // GET: /Article/Create
+        [NeedEditorRole]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Article/Create
+        [NeedEditorRole]
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            try
+            {
+                using (var db = new ArticleEntities())
+                {
+                    article.CreateDate = DateTime.Now;
+                    article.EditDate = DateTime.Now;
+                    article.EditUser = User.Identity.Name;
+                    article.Text = HttpUtility.UrlDecode(article.Text, System.Text.Encoding.Default);
+                    db.Articles.Add(article);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Article/Details/5
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new ArticleEntities())
+            {
+                return View(db.Articles.Find(id));
+            }
+        }
+
+        //
+        // GET: /Article/Edit/5
+        [NeedEditorRole]
+        public ActionResult Edit(int id)
+        {
+            using (var db = new ArticleEntities())
+            {
+                return View(db.Articles.Find(id));
+            }
+        }
+
+        //
+        // POST: /Article/Edit/5
+        [NeedEditorRole]
+        [HttpPost]
+        public ActionResult Edit(int id, Article article)
+        {
+            try
+            {
+                using (var db = new ArticleEntities())
+                {
+                    article.EditDate = DateTime.Now;
+                    article.EditUser = User.Identity.Name;
+                    article.Text = HttpUtility.UrlDecode(article.Text, System.Text.Encoding.Default);
+                    db.Entry(article).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Article/Delete/5
+        [NeedEditorRole]
+        public ActionResult Delete(int id)
+        {
+            using (var db = new ArticleEntities())
+            {
+                return View(db.Articles.Find(id));
+            }
+        }
+
+        //
+        // POST: /Article/Delete/5
+        [NeedEditorRole]
+        [HttpPost]
+        public ActionResult Delete(int id, Article article)
+        {
+            try
+            {
+                using (var db = new ArticleEntities())
+                {
+                    db.Entry(article).State = EntityState.Deleted;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
